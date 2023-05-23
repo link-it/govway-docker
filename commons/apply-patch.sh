cfc7807f85c07badb5f826dd2c649c32f1832cbc
Andrea Manca <manca@link.it>
2023-05-23 17:55:40 +0200

    Applicazione di patch all'immagine con gestione di properties esistenti

--- commons/apply-patch.sh
+++ commons/apply-patch.sh
@@ -5,18 +5,45 @@ do
     ARCHIVENAME="$(basename ${ARCHIVETOPATCH})"
     if [ -d "/opt/PATCH/${ARCHIVENAME}-${GOVWAY_FULLVERSION}.patch" -o -L "/opt/PATCH/${ARCHIVENAME}-${GOVWAY_FULLVERSION}.patch" ] 
     then
-        # Incorporo nuovi files
-        cd "/opt/PATCH/${ARCHIVENAME}-${GOVWAY_FULLVERSION}.patch"
-        zip -r "${ARCHIVETOPATCH}" *; 
-        # Applico patch testuali se presenti
+
         unzip "${ARCHIVETOPATCH}" -d "/tmp/${ARCHIVENAME}.temp"
-        cd "/tmp/${ARCHIVENAME}.temp"
-        for p in $(find . -name \*.diff)
+        
+        cd "/opt/PATCH/${ARCHIVENAME}-${GOVWAY_FULLVERSION}.patch"
+
+        # Applico patch testuali se presenti
+        for d in $(find . -name \*.diff)
         do
-            dos2unix $p
-            patch -p0 < $p
+            /bin/cp -f "${d}" "/tmp/${ARCHIVENAME}.temp/${d}"
+            dos2unix "${d}"
+            pushd "/tmp/${ARCHIVENAME}.temp/"
+            patch -p0 < "${d}"
+            popd
         done
+
+        # Incorporo nuovi properties o aggiorno i presenti
+        for p in $(find . -name \*.properties)
+        do 
+            if [ -f "/tmp/${ARCHIVENAME}.temp/${p}" ] 
+            then
+                cat "${p}" >> "/tmp/${ARCHIVENAME}.temp/${p}"
+            else
+                # assicuro la presenza di tutte le sottodirectory
+                mkdir -p "/tmp/${ARCHIVENAME}.temp/$(dirname ${p})"
+                /bin/cp -f "${p}" "/tmp/${ARCHIVENAME}.temp/${p}"
+            fi
+        done
+        
+        # Incorporo tutti gli eventuali altri files
+        for f in $(find . -not -name \*.properties -and -not -name \*.diff)
+        do 
+            # assicuro la presenza di tutte le sottodirectory
+            mkdir -p "/tmp/${ARCHIVENAME}.temp/$(dirname ${f})"
+            /bin/cp -f "${f}" "/tmp/${ARCHIVENAME}.temp/${f}"
+        done
+
+        cd "/tmp/${ARCHIVENAME}.temp/"
         zip -r  "${ARCHIVETOPATCH}" *
+        cd ..
         rm -rf "/tmp/${ARCHIVENAME}.temp"
     fi
 done
